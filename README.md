280d0dbe8cb7c89a41a0299aade483b710e6d30f
Patrick <ppevans11@gmail.com>
2020-09-12 15:52:27 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Node Template
+
+> A Repository Template for `NodeJS` Projects.
+
+---
+
+## Links & Resources
+
+* [`commitlint`](https://github.com/conventional-changelog/commitlint) Lint commit messages.
+  * [`commitlint-config-gitmoji`](https://github.com/arvinxx/commitlint-config-gitmoji) Commitlint config enforcing gitmoji rules.
+  * [Rules](https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md)
+* [`husky`](https://github.com/typicode/husky) Git hooks made easy.
+* [`np`](https://github.com/sindresorhus/np) A better npm publish.
+
+---
+
+## Project Structure
+
+```md
+.
+├── bin
+│   └── checkUpdates.js  # Check for outdated npm modules
+├── .editorconfig        # Editor configuration
+├── .gitignore           # Git ignore rules
+├── .np-config.json      # NPM publish (np) configuration
+├── LICENSE              # Project LICENSE
+├── package.json         # NPM package configuration
+└── README.md            # Project README
+```
+
+---
---
+++ bin/checkUpdates.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+/**
+ *  bin/checkUpdates.js
+ */
+
+/*************
+ * * IMPORTS *
+ *************/
+
+const childProcess = require("child_process")
+const os = require("os")
+
+const chalk = require('chalk');
+
+/***************
+ * * CONSTANTS *
+ ***************/
+
+const log = console.log;
+
+// Text Styles
+const info = chalk.bgGreen.black;
+const err = chalk.bgRed.black;
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+/**
+ * Check for outdated node modules
+ */
+
+log(info("\n Checking for Outdated Node Modules \n"));
+
+[
+  // TODO: Check yarn.lock exists else run `npm outdated`
+  { command: "yarn outdated & echo." },
+
+  { command: "npx -q npm-check-updates --packageFile package.json & echo." },
+
+  { command: "npx -q updates & echo." }
+]
+  .filter(({ onlyPlatforms }) => !onlyPlatforms || onlyPlatforms.includes(os.platform()))
+  .forEach(commandAndOptions => {
+    const { command, onlyPlatform: _, ...options } = commandAndOptions
+    try {
+      childProcess.execSync(command, {
+        stdio: "inherit",
+        ...options,
+      })
+    } catch (error) {
+      log(err(" ERROR ") + error)
+      // process.exit(error.status)
+    }
+  })
+
+log(info(" DONE \n"))
+
+/* EOF */
